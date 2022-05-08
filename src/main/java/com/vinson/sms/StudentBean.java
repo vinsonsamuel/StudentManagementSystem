769a709611a71d8895187989a21ccbfe07a674dd
Vinson Samuel P <66589579+vinsonsamuel@users.noreply.github.com>
2022-05-08 23:44:47 +0530

    Add files via upload

---
+++ src/main/java/com/vinson/sms/StudentBean.java
@@ -0,0 +1,26 @@
+package com.vinson.sms;
+
+public class StudentBean {
+    String name;
+    String regno;
+    String phno;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getRegno(){
+        return regno;
+    }
+    public void setRegno(String regno){
+        this.regno = regno;
+    }
+    public String getPhno(){
+        return phno;
+    }
+    public void setPhno(String phno){
+        this.phno = phno;
+    }
+}
---
+++ src/main/java/com/vinson/sms/StudentDAO.java
@@ -0,0 +1,77 @@
+package com.vinson.sms;
+
+import java.security.spec.ECField;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class StudentDAO {
+    static Connection conn;
+    static PreparedStatement pst;
+
+    public static int insertStudent(StudentBean u){
+        int status = 0;
+        String sql = "INSERT INTO studentinformation(\"Stu_Name\", \"Stu_Reg_No\", \"Stu_Phone\")" + "VALUES (?, ?, ?)";
+        try{
+            conn = ConnectionProvider.getCon();
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, u.getName());
+            pst.setString(2, u.getRegno());
+            pst.setString(3, u.getPhno());
+            status = pst.executeUpdate();
+            conn.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return status;
+    }
+
+    public static int deleteStudent(StudentBean u){
+        int status = 0;
+        String sql = "DELETE FROM studentinformation WHERE Stu_Reg_No = ?";
+        try{
+            conn = ConnectionProvider.getCon();
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, u.getRegno());
+            status = pst.executeUpdate();
+            conn.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return status;
+    }
+
+    public static int searchStudent(StudentBean u){
+        int status = 0;
+        String sql = "SELECT * from studentinformation WHERE Stu_Reg_No = ?";
+        try{
+           conn = ConnectionProvider.getCon();
+           pst = conn.prepareStatement(sql);
+           System.out.println(pst.execute());
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return status;
+    }
+
+    public static int updateStudent(StudentBean u){
+        int status = 0;
+        String sql = "INSERT INTO studentinformation(\"Stu_Name\", \"Stu_Reg_No\", \"Stu_Phone\")" + "VALUES (?, ?, ?)";
+        try{
+            conn = ConnectionProvider.getCon();
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, u.getName());
+            pst.setString(2, u.getRegno());
+            pst.setString(3, u.getPhno());
+            status = pst.executeUpdate();
+            conn.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return status;
+    }
+
+}

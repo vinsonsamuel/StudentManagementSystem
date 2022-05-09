37ed3fa0704e7f841dcddf4db790e88d254e4f8d
Vinson Samuel P <66589579+vinsonsamuel@users.noreply.github.com>
2022-05-09 16:55:24 +0530

    Update StudentDAO.java

--- src/main/java/com/vinson/sms/StudentDAO.java
+++ src/main/java/com/vinson/sms/StudentDAO.java
@@ -3,6 +3,7 @@ package com.vinson.sms;
 import java.security.spec.ECField;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class StudentDAO {
     static Connection conn;
@@ -42,35 +43,17 @@ public class StudentDAO {
         return status;
     }
 
-    public static int searchStudent(StudentBean u){
-        int status = 0;
-        String sql = "SELECT * from studentinformation WHERE Stu_Reg_No = ?";
-        try{
-           conn = ConnectionProvider.getCon();
-           pst = conn.prepareStatement(sql);
-           System.out.println(pst.execute());
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-        return status;
-    }
-
     public static int updateStudent(StudentBean u){
-        int status = 0;
-        String sql = "UPDATE studentinformation " + "SET Stu_Name = ?, Stu_Phone = ? "
-                + "WHERE Stu_Reg_No = ?";
-        try{
-            conn = ConnectionProvider.getCon();
-            pst = conn.prepareStatement();
-            ResultSet rst = pst.executeQuery(sql)'
-            rst.getString("Stu_Name");
-            rst.getString("Stu_RegNo");
-            rst.getString("Stu_Phone");
-            status = pst.executeUpdate();
+        int status=0;
+        try {
+            conn=ConnectionProvider.getCon();
+            pst= conn.prepareStatement("update studentinformation set Stu_Name=?, Stu_Phone=? where Stu_Reg_No=?");
+            pst.setString(1, u.getName());
+            pst.setString(2, u.getPhno());
+            pst.setString(3, u.getRegno());
+            status=pst.executeUpdate();
             conn.close();
-        }
-        catch (Exception e){
+        }catch(Exception e) {
             System.out.println(e);
         }
         return status;
